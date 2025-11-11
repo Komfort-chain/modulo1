a4b685b109bfa73a07ed0521912c5138d44fff1f
MagyoDev <magyodev@gmail.com>
2025-11-11 12:18:11 -0300

    test: adiciona perfil H2 em memória para testes automatizados no CI

--- pessoas/src/test/java/com/cabos/pessoas/PessoasApplicationTests.java
+++ pessoas/src/test/java/com/cabos/pessoas/PessoasApplicationTests.java
@@ -2,12 +2,14 @@ package com.cabos.pessoas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = PessoasApplication.class)
+@ActiveProfiles("test")
 class PessoasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Teste de inicialização básica da aplicação (sem DB real)
+    }
 }
